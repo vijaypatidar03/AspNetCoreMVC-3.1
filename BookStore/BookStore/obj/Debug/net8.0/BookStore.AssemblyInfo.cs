0170f77f82b069effdab02a6d3bd35a7739d407d
vijaypatidar03 <vijaypatidar9179@gmail.com>
2024-03-03 19:41:40 +0530

    Created a project in8 version

--- BookStore/BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
+++ BookStore/BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b2a59b4b4d4253bf4ad523e2afb1ed3dadc8535")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
